9d392cd2e9871c5673893e76cd4703cd6d51bcbf
dengqi <dqasd00071801@oulook.com>
2025-01-08 10:30:55 +0800

    commit_usart

---
+++ 1.0.1_usart/USER/main.c
@@ -0,0 +1,163 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+#include "FreeRTOS.h"
+#include "task.h"
+/************************************************
+ ALIENTEK 探索者STM32F407开发板 FreeRTOS实验2-1
+ FreeRTOS移植实验-库函数版本
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+
+//任务优先级
+#define START_TASK_PRIO		1
+//任务堆栈大小	
+#define START_STK_SIZE 		128  
+//任务句柄
+TaskHandle_t StartTask_Handler;
+//任务函数
+void start_task(void *pvParameters);
+
+//任务优先级
+#define LED0_TASK_PRIO		2
+//任务堆栈大小	
+#define LED0_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED0Task_Handler;
+//任务函数
+void led0_task(void *pvParameters);
+
+//任务优先级
+#define LED1_TASK_PRIO		3
+//任务堆栈大小	
+#define LED1_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED1Task_Handler;
+//任务函数
+void led1_task(void *pvParameters);
+
+//任务优先级
+#define FLOAT_TASK_PRIO		4
+//任务堆栈大小	
+#define FLOAT_STK_SIZE 		128
+//任务句柄
+TaskHandle_t FLOATTask_Handler;
+//任务函数
+void float_task(void *pvParameters);
+
+int main(void)
+{ 
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4
+	delay_init(168);		//初始化延时函数
+	uart_init(115200);     	//初始化串口
+	LED_Init();		        //初始化LED端口
+	
+	//创建开始任务
+    xTaskCreate((TaskFunction_t )start_task,            //任务函数
+                (const char*    )"start_task",          //任务名称
+                (uint16_t       )START_STK_SIZE,        //任务堆栈大小
+                (void*          )NULL,                  //传递给任务函数的参数
+                (UBaseType_t    )START_TASK_PRIO,       //任务优先级
+                (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
+    vTaskStartScheduler();          //开启任务调度
+}
+ 
+//开始任务任务函数
+void start_task(void *pvParameters)
+{
+    taskENTER_CRITICAL();           //进入临界区
+    //创建LED0任务
+    xTaskCreate((TaskFunction_t )led0_task,     	
+                (const char*    )"led0_task",   	
+                (uint16_t       )LED0_STK_SIZE, 
+                (void*          )NULL,				
+                (UBaseType_t    )LED0_TASK_PRIO,	
+                (TaskHandle_t*  )&LED0Task_Handler);   
+    //创建LED1任务
+    xTaskCreate((TaskFunction_t )led1_task,     
+                (const char*    )"led1_task",   
+                (uint16_t       )LED1_STK_SIZE, 
+                (void*          )NULL,
+                (UBaseType_t    )LED1_TASK_PRIO,
+                (TaskHandle_t*  )&LED1Task_Handler);        
+    //浮点测试任务
+    xTaskCreate((TaskFunction_t )float_task,     
+                (const char*    )"float_task",   
+                (uint16_t       )FLOAT_STK_SIZE, 
+                (void*          )NULL,
+                (UBaseType_t    )FLOAT_TASK_PRIO,
+                (TaskHandle_t*  )&FLOATTask_Handler);  
+    vTaskDelete(StartTask_Handler); //删除开始任务
+    taskEXIT_CRITICAL();            //退出临界区
+}
+
+//LED0任务函数 
+void led0_task(void *pvParameters)
+{
+    while(1)
+    {
+        LED0=~LED0;
+        vTaskDelay(500);
+    }
+}   
+
+//LED1任务函数
+void led1_task(void *pvParameters)
+{
+    while(1)
+    {
+        LED1=~LED1;
+        vTaskDelay(500);
+    }
+}
+
+//浮点测试任务
+void float_task(void *pvParameters)
+{
+//	static float float_num=0.00;
+//	while(1)
+//	{
+//		float_num+=0.01f;
+//		printf("float_num的值为: %.4f\r\n",float_num);
+//        vTaskDelay(1000);
+//	}
+	
+		u8 t;
+	u8 len;	
+	u16 times=0;  
+		while(1)
+	{
+		if(USART_RX_STA&0x8000)
+		{					   
+			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
+			printf("\r\n您发送的消息为:\r\n");
+			for(t=0;t<len;t++)
+			{
+				USART_SendData(USART3, USART_RX_BUF[t]);         //向串口1发送数据
+				while(USART_GetFlagStatus(USART3,USART_FLAG_TC)!=SET);//等待发送结束
+			}
+			printf("\r\n\r\n");//插入换行
+			USART_RX_STA=0;
+		}else
+		{
+			times++;
+			if(times%5000==0)
+			{
+				printf("\r\nALIENTEK 探索者STM32F407开发板 串口实验\r\n");
+				printf("正点原子@ALIENTEK\r\n\r\n\r\n");
+				
+			}
+			if(times%200==0)printf("请输入数据,以回车键结束\r\n");  
+			if(times%30==0)LED0=!LED0;//闪烁LED,提示系统正在运行.
+			delay_ms(10);   
+		}
+		printf("TIMES=%d\n",times);
+	}
+}
+
+
